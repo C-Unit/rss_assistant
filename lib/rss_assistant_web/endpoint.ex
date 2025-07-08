31b49342dd345d206ec0ec66535b18591e40e009
Hannah Carnes <32318292+hcarnes@users.noreply.github.com>
2025-07-08 23:25:39 +0000

    format

--- lib/rss_assistant_web/endpoint.ex
+++ lib/rss_assistant_web/endpoint.ex
@@ -42,6 +42,7 @@ defmodule RssAssistantWeb.Endpoint do
   plug Plug.Telemetry, event_prefix: [:phoenix, :endpoint]
 
   plug Plug.Parsers,
+  
     parsers: [:urlencoded, :multipart, :json],
     pass: ["*/*"],
     json_decoder: Phoenix.json_library()
